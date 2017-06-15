0cf5ba9ecfa794abc40251eaec8c2a3f3aa23c0c
Federico Tomasi <fdtomasi@gmail.com>
2017-06-15 14:10:39 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+"""RGI   setup script.
+
+Author: Federico Tomasi
+Copyright (c) 2017, Federico Tomasi.
+Licensed under the FreeBSD license (see LICENSE.txt).
+"""
+
+from setuptools import setup, Extension
+import numpy as np
+
+# Package Version
+from rgi import __version__ as version
+# alignment_module = Extension('icing.align.align',
+#                              sources=['icing/align/alignment.c'])
+setup(
+    name='rgi',
+    version=version,
+
+    description=('RGI (Regularised Graph Inference)'),
+    long_description=open('README.md').read(),
+    author='Federico Tomasi',
+    author_email='federico.tomasi@dibris.unige.it',
+    maintainer='Federico Tomasi',
+    maintainer_email='federico.tomasi@dibris.unige.it',
+    url='https://github.com/fdtomasi/rgi',
+    download_url='https://github.com/fdtomasi/rgi/archive/'+version+'.tar.gz',
+    keywords=['graph', 'inference'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+    license='FreeBSD',
+    packages=['rgi'],
+    requires=['numpy (>=1.10.1)',
+              'scipy (>=0.16.1)',
+              'sklearn (>=0.17)',
+              'matplotlib (>=1.5.1)',
+              'seaborn (>=0.7.0)'],
+    # scripts=['scripts/ici_run.py', 'scripts/ici_analysis.py'],
+    # ext_modules=[ssk_module],
+    include_dirs=[np.get_include()]
+)
