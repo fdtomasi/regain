6a460e159f929d75af14b8c841950ff8a3af7c87
Federico Tomasi <fdtomasi@gmail.com>
2017-11-03 17:29:52 +0100

    fix non latent algorithm

--- regain/time_graph_lasso_admm.py
+++ regain/time_graph_lasso_admm.py
@@ -15,6 +15,7 @@ from sklearn.utils.extmath import fast_logdet, squared_norm
 
 from regain.norm import l1_od_norm
 from regain.prox import prox_logdet, prox_laplacian, soft_thresholding_sign
+from regain.prox import soft_thresholding_od
 from regain.utils import convergence
 
 
@@ -32,7 +33,7 @@ def objective(n_samples, S, K, Z_0, Z_1, Z_2, lamda, beta, psi):
     return obj
 
 
-def covseltime(
+def time_graph_lasso(
         data_list, lamda=1, rho=1, beta=1,
         max_iter=1000, verbose=False,
         tol=1e-4, rtol=1e-2, return_history=False):
@@ -75,10 +76,10 @@ def covseltime(
     S = np.array(map(empirical_covariance, data_list))
     n_samples = np.array([s.shape[0] for s in data_list])
 
-    Theta = np.zeros_like(S)
-    # Z_0 = np.zeros_like(Theta)
-    # Z_1 = np.zeros_like(Theta)[:-1]
-    # Z_2 = np.zeros_like(Theta)[1:]
+    # K = np.zeros_like(S)
+    Z_0 = np.zeros_like(S)
+    Z_1 = np.zeros_like(S)[:-1]
+    Z_2 = np.zeros_like(S)[1:]
     U_0 = np.zeros_like(S)
     U_1 = np.zeros_like(S)[:-1]
     U_2 = np.zeros_like(S)[1:]
@@ -89,58 +90,66 @@ def covseltime(
 
     # divisor for consensus variables, accounting for two less matrices
     divisor = np.zeros(S.shape[0]) + 3
-    divisor[0] = divisor[-1] = 2
-    eta = np.divide(n_samples, divisor * rho)
+    divisor[0] -= 1
+    divisor[-1] -= 1
+    # eta = np.divide(n_samples, divisor * rho)
 
     checks = []
     for _ in range(max_iter):
         # x-update
-        A = Z_consensus - U_consensus
+        # A = Z_consensus - U_consensus
+        A = Z_0 - U_0
+        A[:-1] += Z_1 - U_1
+        A[1:] += Z_2 - U_2
+
         A += np.array(map(np.transpose, A))
         A /= 2.
-        A /= eta[:, np.newaxis, np.newaxis]
-        A -= S
-        Theta = np.array(map(prox_logdet, A, eta))
+
+        # A /= eta[:, np.newaxis, np.newaxis]
+        A *= - rho / n_samples[:, np.newaxis, np.newaxis]
+        A += S
+
+        K = np.array(map(prox_logdet, A, n_samples / (rho * divisor)))
 
         # z-update with relaxation
         # Zold = Z
         # X_hat = alpha * X + (1 - alpha) * Zold
-        soft_thresholding = partial(soft_thresholding_sign, lamda=lamda / rho)
-        Z_0 = np.array(map(soft_thresholding, Theta + U_0))
+        soft_thresholding = partial(soft_thresholding_od, lamda=lamda / rho)
+        Z_0 = np.array(map(soft_thresholding, K + U_0))
 
         # other Zs
         # prox_l = partial(prox_laplacian, beta=2. * beta / rho)
         # prox_e = np.array(map(prox_l, Theta[1:] - Theta[:-1] + U_2 - U_1))
-        prox_e = prox_laplacian(Theta[1:] - Theta[:-1] + U_2 - U_1,
+        prox_e = prox_laplacian(K[1:] - K[:-1] + U_2 - U_1,
                                 beta=2. * beta / rho)
-        Z_1 = .5 * (Theta[:-1] + Theta[1:] + U_1 + U_2 - prox_e)
-        Z_2 = .5 * (Theta[:-1] + Theta[1:] + U_1 + U_2 + prox_e)
+        Z_1 = .5 * (K[:-1] + K[1:] + U_1 + U_2 - prox_e)
+        Z_2 = .5 * (K[:-1] + K[1:] + U_1 + U_2 + prox_e)
 
-        U_0 += (Theta - Z_0)
-        U_1 += (Theta[:-1] - Z_1)
-        U_2 += (Theta[1:] - Z_2)
+        U_0 += (K - Z_0)
+        U_1 += (K[:-1] - Z_1)
+        U_2 += (K[1:] - Z_2)
 
         # diagnostics, reporting, termination checks
         Z_consensus = Z_0.copy()
         Z_consensus[:-1] += Z_1
         Z_consensus[1:] += Z_2
+        Z_consensus /= divisor[:, np.newaxis, np.newaxis]
+
         U_consensus = U_0.copy()
         U_consensus[:-1] += U_1
         U_consensus[1:] += U_2
-
-        np.divide(Z_consensus, divisor[:, np.newaxis, np.newaxis], out=Z_consensus)
-        np.divide(U_consensus, divisor[:, np.newaxis, np.newaxis], out=U_consensus)
+        U_consensus /= divisor[:, np.newaxis, np.newaxis]
 
         check = convergence(
-            obj=objective(n_samples, S, Theta, Z_0, Z_1, Z_2, lamda,
+            obj=objective(n_samples, S, K, Z_0, Z_1, Z_2, lamda,
                           beta, squared_norm),
-            rnorm=np.linalg.norm(Theta - Z_consensus),
+            rnorm=np.linalg.norm(K - Z_consensus),
             snorm=np.linalg.norm(
                 rho * (Z_consensus - Z_consensus_old)),
 
-            e_pri=np.sqrt(np.prod(Theta.shape)) * tol + rtol * max(
-                np.linalg.norm(Theta), np.linalg.norm(Z_consensus)),
-            e_dual=np.sqrt(np.prod(Theta.shape)) * tol + rtol * np.linalg.norm(
+            e_pri=np.sqrt(np.prod(K.shape)) * tol + rtol * max(
+                np.linalg.norm(K), np.linalg.norm(Z_consensus)),
+            e_dual=np.sqrt(np.prod(K.shape)) * tol + rtol * np.linalg.norm(
                 rho * U_consensus)
         )
         Z_consensus_old = Z_consensus.copy()
@@ -156,5 +165,5 @@ def covseltime(
         warnings.warn("Objective did not converge.")
 
     if return_history:
-        return Theta, S, checks
-    return Theta, S
+        return K, S, checks
+    return K, S
