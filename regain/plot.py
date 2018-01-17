bf4b68237bc0333eac18694ad388033bf6faf628
Federico Tomasi <fdtomasi@gmail.com>
2018-01-17 09:45:23 +0100

    networkx is now optional

--- regain/plot.py
+++ regain/plot.py
@@ -1,12 +1,12 @@
 """Plotting utils."""
 import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
 
 
 def plot_graph_with_latent_variables(adjacency_matrix, n_latents, n_observed,
                                      title):
     """Plot graph with latent variables."""
+    import networkx as nx
     # plt.figure(figsize=(15,10))
     plt.figure()
     G = nx.from_numpy_matrix(adjacency_matrix)
