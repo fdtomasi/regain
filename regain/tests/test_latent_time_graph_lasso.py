8ec20660efcd6557bb8519908e7c795408bd2e4c
Federico Tomasi <fdtomasi@gmail.com>
2018-10-10 19:37:22 +0200

    add coverage tests

--- regain/tests/test_latent_time_graph_lasso.py
+++ regain/tests/test_latent_time_graph_lasso.py
@@ -22,5 +22,25 @@ def test_ltgl_zero():
 
     assert_array_equal(mdl.precision_, a)
     assert_array_equal(mdl.latent_, a)
-    assert_array_equal(mdl.get_observed_precision(),
-                       mdl.precision_ - mdl.latent_)
+    assert_array_equal(
+        mdl.get_observed_precision(), mdl.precision_ - mdl.latent_)
+
+
+def test_ltgl_prox_l1():
+    """Check that LatentTimeGraphLasso can handle zero data."""
+    a = np.zeros((3, 3, 3))
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        mdl = LatentTimeGraphLasso(
+            time_on_axis='first', psi='l2', phi='node', max_iter=1,
+            assume_centered=True).fit(a)
+
+    for p in mdl.precision_:
+        # remove the diagonal
+        p.flat[::4] = 0
+
+    assert_array_equal(mdl.precision_, a)
+    assert_array_equal(mdl.latent_, a)
+    assert_array_equal(
+        mdl.get_observed_precision(), mdl.precision_ - mdl.latent_)
