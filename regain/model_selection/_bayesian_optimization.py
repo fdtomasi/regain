607671a6c76e3b4589caea9d1c3271fcac77d1a6
Federico Tomasi <fdtomasi@gmail.com>
2019-12-23 12:21:03 +0100

    add license in model selection

--- regain/model_selection/_bayesian_optimization.py
+++ regain/model_selection/_bayesian_optimization.py
@@ -1,3 +1,32 @@
+# BSD 3-Clause License
+
+# Copyright (c) 2017, Federico T.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """Deprecated.
 
 This module to port GPyOpt in a `sklearn-usable` way has been deprecated
@@ -22,7 +51,7 @@ from GPyOpt.optimization.acquisition_optimizer import AcquisitionOptimizer
 
 
 @deprecated()
-class BayesianOptimization(GPyOpt.methods.BayesianOptimization, BaseSearchCV):
+class _BayesianOptimization(GPyOpt.methods.BayesianOptimization, BaseSearchCV):
     """Wrapper for BayesianOptimization object from GPyOpt package.
 
     This is done to use estimator in the same way as scikit-learn.
